983d727da41b6647b5ea6b326138278c194e5a29
Alexei196 <dndy778@gmail.com>
2023-04-07 15:42:09 -0700

    new way of kmeans

--- kmeans.cpp
+++ kmeans.cpp
@@ -11,8 +11,7 @@ int main(int argc, char** argv ) {
     }
 
     //load image into matrix
-    Mat image;
-    image = imread( argv[1], 1 );
+    Mat image = imread( argv[1], 1 );
     if ( !image.data ) {
         printf("No image data \n");
         return -1;
@@ -23,31 +22,9 @@ int main(int argc, char** argv ) {
         Scalar(0, 255, 0),
         Scalar(0, 0, 255)
     };
-
-    bool loop = true;
-    int i, k;
-    int sampleCount, clusterCount = 3;
-    Mat points(sampleCount, 1, CV_32FC2), labels;
-    std::vector<Point2f> centers;
-    //kmeans function call from OpenCV 
-    double compactness = kmeans(image, clusterCount, labels, 
-    TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0), 
-    3, KMEANS_PP_CENTERS, centers);
-
-    for(i = 0; i < sampleCount; ++i) {
-        int clusterIdx = labels.at<int>(i);
-        Point ipt = points.at<Point2f>(i);
-        circle(image, ipt, 2, colorTab[clusterIdx], FILLED, LINE_AA);
-    }
-
-    for(i = 0; i < (int) centers.size(); ++i) {
-        Point2f c = centers[i];
-        circle(image, c, 40, colorTab[i], 1, LINE_AA);
-    }
-
-    //display results
-    std::cout << "Compactness: " << compactness << "\n"; 
-    imshow("clusters", image);
+    //
+    int clustersCount = 3, iterations = 5;
+    Mat segmentsImage = imageQuantization(image, clustersCount, iterations);
 
     
     namedWindow("Display Image", WINDOW_AUTOSIZE );
