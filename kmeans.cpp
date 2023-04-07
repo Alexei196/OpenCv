165f02e82b5f9e00ee568f2887bcac765e9d00f5
Alexei196 <dndy778@gmail.com>
2023-04-07 15:29:51 -0700

    cmake stuff

--- kmeans.cpp
+++ kmeans.cpp
@@ -1,33 +1,59 @@
-#include <stdio.h>
+#include <iostream>
+#include<vector>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 
 int main(int argc, char** argv ) {
     if ( argc != 2 ) {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("usage: kmeans.exe <Image_Path>\n");
         return -1;
     }
 
+    //load image into matrix
     Mat image;
     image = imread( argv[1], 1 );
     if ( !image.data ) {
         printf("No image data \n");
         return -1;
     }
+    //Setup for k means analysis
+    Scalar colorTab[] = {
+        Scalar(255, 0, 0),
+        Scalar(0, 255, 0),
+        Scalar(0, 0, 255)
+    };
 
+    bool loop = true;
+    int i, k;
+    int sampleCount, clusterCount = 3;
+    Mat points(sampleCount, 1, CV_32FC2), labels;
+    std::vector<Point2f> centers;
+    //kmeans function call from OpenCV 
+    double compactness = kmeans(image, clusterCount, labels, 
+    TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0), 
+    3, KMEANS_PP_CENTERS, centers);
+
+    for(i = 0; i < sampleCount; ++i) {
+        int clusterIdx = labels.at<int>(i);
+        Point ipt = points.at<Point2f>(i);
+        circle(image, ipt, 2, colorTab[clusterIdx], FILLED, LINE_AA);
+    }
+
+    for(i = 0; i < (int) centers.size(); ++i) {
+        Point2f c = centers[i];
+        circle(image, c, 40, colorTab[i], 1, LINE_AA);
+    }
+
+    //display results
+    std::cout << "Compactness: " << compactness << "\n"; 
+    imshow("clusters", image);
+
+    
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
 
-    kmeans(
-        image, //InputArray  	data,
-		3, //int  	K,
-		labels, //InputOutputArray  	bestLabels,
-		//TermCriteria  	criteria,
-		//int  	attempts,
-		//int  	flags,
-		OutputArray  	centers = noArray()
-    )
+    
     waitKey(0);
     return 0;
 }
--- openCvTestFile.cpp
+++ openCvTestFile.cpp
@@ -1,7 +1,9 @@
 //source:: https://docs.opencv.org/4.7.0/db/df5/tutorial_linux_gcc_cmake.html
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+
 using namespace cv;
+
 int main(int argc, char** argv )
 {
     if ( argc != 2 )
