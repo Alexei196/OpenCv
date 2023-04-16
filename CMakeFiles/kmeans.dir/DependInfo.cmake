2495f4938ca8e7dc5d2838d5dd4cc399ad4b6f5f
Alexei196 <dndy778@gmail.com>
2023-04-16 13:23:23 -0700

    updated kmeans

--- CMakeFiles/kmeans.dir/DependInfo.cmake
+++ CMakeFiles/kmeans.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/andrew/Documents/folder/cvTest/kmeans.cpp" "CMakeFiles/kmeans.dir/kmeans.cpp.o" "gcc" "CMakeFiles/kmeans.dir/kmeans.cpp.o.d"
+  "/home/andrew/Documents/cvTest/kmeans.cpp" "CMakeFiles/kmeans.dir/kmeans.cpp.o" "gcc" "CMakeFiles/kmeans.dir/kmeans.cpp.o.d"
   )
 
 # Targets to which this target links.
--- kmeans.cpp
+++ kmeans.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv ) {
     }
 
     //load image into matrix
-    Mat image = imread( argv[1], 1 );
+    Mat image = imread( argv[1], IMREAD_GRAYSCALE);
     if ( !image.data ) {
         printf("No image data \n");
         return -1;
@@ -25,50 +25,74 @@ int main(int argc, char** argv ) {
         Scalar(0, 0, 255)
     };
     //set up for kmeans
-    const int clustersCount = 3, iterations = 10;
+    const int clustersCount = 4, iterations = 7;
 
     //1. Define random centroids for k clusters
-    int centroids[clustersCount];
+    long long int centroids[clustersCount];
     long long int centroidSum[clustersCount];
     int centroidCount[clustersCount];
     for(int i = 0; i < clustersCount; ++i) {
-        centroids[i] = rand() % 256;
-        centroidSum[i] = 0;
+        centroids[i] = (long long int) (rand() % 256);
+        centroidSum[i] = 0ll;
         centroidCount[i] = 0;
     }
     //2. Assign data to closest centroid
-    int lowestDistance = 65536, closestCentroid = 0;
+    int lowestDistance, closestCentroid;
     //For each iteration of the k-means alg
     for(int i = 0; i < iterations; ++i) {
+        printf("DEBUG ITER: %d\n", i);
         //For each pixel in image
-        for(int y = 0; y < image.rows(); ++y) {
-            for(int x = 0; x < image.cols(); ++x) {     
-                //For each centroid in existence
-                //TODO reset distance and closest centroid
-                for(int c = 0; c < clustersCount; ++c) {
-                    if(distance((int)image.at<int>(y,x), centroids[c]) < lowestDistance) {
-                        closestCentroid = c;
-                        lowestDistance = distance((int)image.at<int>(y,x), centroids[c]);
-                    }
+        for(long int y = 0; y < image.rows; ++y) {
+            for(long int x = 0; x < image.cols; ++x) {
+                if((int)image.at<unsigned char>(y,x) < 24) {
+                    continue;
                 }
-                centroidSum[closestCentroid] += (long long int)image.at<int>(y,x);
+                //For each centroid in existence
+                closestCentroid = 0;
+                lowestDistance = distance((int)image.at<unsigned char>(y,x), centroids[0]);
+                for(int c = 1; c < clustersCount; ++c) {
+                    int space = distance((int)image.at<unsigned char>(y,x), centroids[c]);
+                    if(space < lowestDistance) {
+                        closestCentroid = c;
+                        lowestDistance = space;
+                    }
+                    
+                }
+                centroidSum[closestCentroid] += (long long int)image.at<unsigned char>(y,x);
                 centroidCount[closestCentroid] += 1;
             }
         }
         //3. Assign centroid to average of each grouped data
         for(int c = 0; c < clustersCount; ++c) {
-            centroids[c] = centroidSum[c] / centroidCount[c];
+            centroids[c] = (long long int) (centroidSum[c] / (long long int) centroidCount[c]);
+            printf("Centroid %d: %lld\n", c, centroids[c]); 
         }
     }
-     //4. perform 2 and 3 i amount of times   
+    //4. perform 2 and 3 i amount of times   
 
+    Mat centroidAssigned = image.clone();
+
+    for(long int y = 0; y < image.rows; ++y) {
+        for(long int x = 0; x < image.cols; ++x) {  
+            closestCentroid = 0;
+            lowestDistance = distance((int)image.at<unsigned char>(y,x), centroids[0]);
+            for(int c = 1; c < clustersCount; ++c) {
+                int space = distance((int)image.at<unsigned char>(y,x), centroids[c]);
+                if(space < lowestDistance) {
+                    closestCentroid = c;
+                    lowestDistance = space;
+                }
+            }
+            centroidAssigned.at<unsigned char>(y,x) = closestCentroid * 122;
+        }
+    }
     namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", clusteredImage);
+    imshow("Display Image", centroidAssigned);
 
     waitKey(0);
     return 0;
 }
 
 int distance(const int &l1, const int &l2) {
-    return l2 - l1;
+    return (l2 - l1) < 0 ? -1*(l2-l1) : (l2-l1);
 }
