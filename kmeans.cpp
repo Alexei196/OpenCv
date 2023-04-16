863265af4b4743b6d332be9272079295bd094d0c
Alexei196 <dndy778@gmail.com>
2023-04-16 16:32:45 -0700

    functional kMeans

--- kmeans.cpp
+++ kmeans.cpp
@@ -5,6 +5,7 @@
 using namespace cv;
 
 int distance(const int&, const int&);
+Mat kMeans(const Mat&, const int&, const int&);
 
 int main(int argc, char** argv ) {
     if ( argc != 2 ) {
@@ -19,31 +20,35 @@ int main(int argc, char** argv ) {
         return -1;
     }
     //Setup for k means analysis
-    Scalar colorTab[] = {
-        Scalar(255, 0, 0),
-        Scalar(0, 255, 0),
-        Scalar(0, 0, 255)
-    };
     //set up for kmeans
-    const int clustersCount = 4, iterations = 7;
+    kMeans(image, 4, 7);
+    return 0;
+}
 
+int distance(const int &l1, const int &l2) {
+    return (l2 - l1) < 0 ? -1*(l2-l1) : (l2-l1);
+}
+
+Mat kMeans(const Mat& image, const int& clustersCount, const int& iterations) {
     //1. Define random centroids for k clusters
-    long long int centroids[clustersCount];
     long long int centroidSum[clustersCount];
-    int centroidCount[clustersCount];
+    int centroids[clustersCount], centroidCount[clustersCount];
     for(int i = 0; i < clustersCount; ++i) {
-        centroids[i] = (long long int) (rand() % 256);
+        centroids[i] = (int) (rand() % 256);
         centroidSum[i] = 0ll;
         centroidCount[i] = 0;
     }
+
     //2. Assign data to closest centroid
+    Mat centroidAssigned = image.clone();
     int lowestDistance, closestCentroid;
+    int colorScale = 256 / (clustersCount - 1);
     //For each iteration of the k-means alg
     for(int i = 0; i < iterations; ++i) {
-        printf("DEBUG ITER: %d\n", i);
         //For each pixel in image
         for(long int y = 0; y < image.rows; ++y) {
             for(long int x = 0; x < image.cols; ++x) {
+                //centroids ignore all black pixels
                 if((int)image.at<unsigned char>(y,x) < 24) {
                     continue;
                 }
@@ -56,43 +61,30 @@ int main(int argc, char** argv ) {
                         closestCentroid = c;
                         lowestDistance = space;
                     }
-                    
                 }
+                //Now that centroids are found, replace the pixels with the color of the centroid
+                centroidAssigned.at<unsigned char>(y,x) = closestCentroid * colorScale;
                 centroidSum[closestCentroid] += (long long int)image.at<unsigned char>(y,x);
                 centroidCount[closestCentroid] += 1;
             }
         }
-        //3. Assign centroid to average of each grouped data
+        //3. Assign centroid to the average of each grouped data
         for(int c = 0; c < clustersCount; ++c) {
+            if(centroidCount[c] == 0) {
+                //In event centroid is not counted
+                fprintf(stderr, "Centroid %d did not gain any points!\n", c);
+                continue;
+            }
             centroids[c] = (long long int) (centroidSum[c] / (long long int) centroidCount[c]);
             printf("Centroid %d: %lld\n", c, centroids[c]); 
         }
     }
     //4. perform 2 and 3 i amount of times   
 
-    Mat centroidAssigned = image.clone();
+    namedWindow("Before Image", WINDOW_AUTOSIZE );
+    imshow("Before Image", centroidAssigned);
 
-    for(long int y = 0; y < image.rows; ++y) {
-        for(long int x = 0; x < image.cols; ++x) {  
-            closestCentroid = 0;
-            lowestDistance = distance((int)image.at<unsigned char>(y,x), centroids[0]);
-            for(int c = 1; c < clustersCount; ++c) {
-                int space = distance((int)image.at<unsigned char>(y,x), centroids[c]);
-                if(space < lowestDistance) {
-                    closestCentroid = c;
-                    lowestDistance = space;
-                }
-            }
-            centroidAssigned.at<unsigned char>(y,x) = closestCentroid * 122;
-        }
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", centroidAssigned);
-
-    waitKey(0);
-    return 0;
-}
-
-int distance(const int &l1, const int &l2) {
-    return (l2 - l1) < 0 ? -1*(l2-l1) : (l2-l1);
+    namedWindow("Kmeans Image", WINDOW_AUTOSIZE );
+    imshow("Kmeans Image", centroidAssigned);
+    return centroidAssigned;
 }
